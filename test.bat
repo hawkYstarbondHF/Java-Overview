13f97e968c0ba7963c0e26961b7315f3b748fb07
hawkYstarbondHF <elle.c.wagner@outlook.com>
2025-05-13 10:40:00 -0500

    Create test.bat

---
+++ test.bat
@@ -0,0 +1,4 @@
+del *.class
+javac JavaOverview.java
+javac -cp ".;junit-platform-console-standalone-1.8.2.jar" JavaOverviewTest.java
+java -jar junit-platform-console-standalone-1.8.2.jar --class-path="." --select-class=JavaOverviewTest
